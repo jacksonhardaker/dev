51dff0ffdd6ecffd2b1496c0eb6e6746325704a3
Jackson Hardaker <jackson.hardaker@gmail.com>
2019-12-03 20:51:43 -0500

    feat(favicon): add assets and links

--- components/Page.js
+++ components/Page.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Head from 'next/head';
 import { primary, black } from '../constants/colors';
 
 const Page = ({ children }) => {
   return (
     <>
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+      <meta name="msapplication-TileColor" content="#da532c"></meta>
+      <meta name="theme-color" content="#ffffff"></meta>
+    </Head>
     {children}
     <style jsx global>{`
       @font-face {
