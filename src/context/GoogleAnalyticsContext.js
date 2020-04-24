073cdc846ced9fed2dc24c35633e4e31f6a666ba
Jackson Hardaker <jackson.hardaker@gmail.com>
2020-04-24 14:24:22 -0400

    refactor analytics to use window.ga

--- src/context/GoogleAnalyticsContext.js
+++ src/context/GoogleAnalyticsContext.js
@@ -5,43 +5,40 @@ export const GoogleAnalyticsContext = createContext();
 
 export const GoogleAnalyticsProvider = ({ children }) => {
   const [analyticsEnabled] = useState(JSON.parse(process.env.ENABLE_GOOGLE_ANALYTICS || 'false'));
-  const [analytics, setAnalytics] = useState(null);
+  const [analyticsInitialized, setAnalyticsInitialized] = useState(null);
 
   const analyticsDebug = (...args) => {
     console.log(`GA: ${args}`);
   }
-
-  const firePageView = url => analytics('send', 'pageview', url);
-
+  
+  const firePageView = url => analytics()('send', 'pageview', url);
+  
+  const analytics = () => analyticsEnabled ? window.ga : analyticsDebug;
+  
   const value = {
     analytics,
     firePageView,
   };
 
   useEffect(() => {
-    if (analytics) {
-      analytics('create', 'UA-164498009-1', 'auto');
+    if (analyticsInitialized) {
+      analytics()('create', 'UA-164498009-1', 'auto');
       firePageView();
 
       Router.events.on('routeChangeComplete', firePageView);
     }
-  }, [analytics]);
+  }, [analyticsInitialized]);
 
   useEffect(() => {
-    if (analyticsEnabled) {
-      (function(i, s, o, g, r, a, m) {
+    (function(i, s, o, g, r, a, m) {
       i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function() {
         (i[r].q = i[r].q || []).push(arguments)
       }, i[r].l = 1 * new Date(); a = s.createElement(o),
         m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-  
-      setAnalytics(() => window.ga);
-    }
-    else {
-      setAnalytics(() => analyticsDebug)
-    }
-  }, [analyticsEnabled]);
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    setAnalyticsInitialized(true);
+  }, []);
 
   return (
     <GoogleAnalyticsContext.Provider {...{ value }}>
