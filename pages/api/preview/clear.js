d5c5bc9009bfa18f794ea283444e64d7f1aa6d54
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-02-15 17:49:14 -0500

    remove prismic stuff

--- pages/api/preview/clear.js
+++
@@ -1,7 +0,0 @@
-export default async (req, res) => {
-  const { redirect } = req.query;
-
-  res.clearPreviewData();
-  res.writeHead(307, { Location: redirect || '/' });
-  res.end();
-};
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import PrismicPreviewScript from './PrismicPreviewScript';
-import ClearPreviewMode from './ClearPreviewMode';
-import Nav from './Navigation';
 
 const Page = ({ children, canonical, preview }) => {
   return (
     <>
-      <PrismicPreviewScript preview={preview} />
       <Head>
         <link rel="canonical" href={canonical}></link>
       </Head>
       {children}
-      {preview && <ClearPreviewMode />}
     </>
   )
 };
