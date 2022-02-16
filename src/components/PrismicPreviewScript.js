42db10521f9706069ce0f0bb31c0257adf3eb049
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-02-16 13:35:58 -0500

    remove prismic completely

--- src/components/PrismicPreviewScript.js
+++
@@ -1,7 +0,0 @@
-import Head from 'next/head';
-
-const PrismicPreviewScript = ({ preview }) => {
-  return preview ? <Head><script async defer src="https://static.cdn.prismic.io/prismic.min.js?repo=jackson-hardaker-dev&new=true"></script></Head> : null;
-};
-
-export default PrismicPreviewScript;
