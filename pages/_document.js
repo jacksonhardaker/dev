9d6ffafd5707b701ce1c53acaf480d5b55b8621d
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-02-18 09:50:40 -0500

    refactor to functional component

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class _Document extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
-          <link rel="manifest" href="/site.webmanifest"></link>
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
-          <meta name="msapplication-TileColor" content="#da532c"></meta>
-          <meta name="theme-color" content="#ffffff"></meta>
-          {process.env.META_ROBOTS && <meta name="robots" content={process.env.META_ROBOTS}></meta>}
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+        {process.env.META_ROBOTS && <meta name="robots" content={process.env.META_ROBOTS}></meta>}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-export default _Document;
+export default Document;
