1c5b4dcfcb75ceb247082cb2b218594a17c6f74f
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-02-19 12:42:10 -0500

    convert styles to pure css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { GlobalIntersectionObserverProvider } from '@context/GlobalIntersectionObserver';
 import { ThemeProvider } from '@context/ThemeContext';
 import { GoogleAnalyticsProvider } from '@context/GoogleAnalyticsContext';
 import { Navigation } from '@components/Navigation';
 
 import '../src/loading.config';
+
 import 'normalize.css/normalize.css';
-import '../src/styles/base.scss';
-import { GlobalIntersectionObserverProvider } from '@context/GlobalIntersectionObserver';
+import '../src/styles/colors.css';
+import '../src/styles/loading.css';
+import '../src/styles/base.css';
 
 const MainApp = ({ Component, pageProps }) => {
   const { asPath: canonical } = useRouter();
