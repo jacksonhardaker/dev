f7e3a3c3c295d1e9b4cf16ef204d6e7d0b01486f
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-02-23 21:43:44 -0500

    change to get origin from env

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,31 +1,21 @@
 import NextImage, { ImageLoader, ImageProps } from 'next/image';
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 
-const loader =
-  (origin: string): ImageLoader =>
-  ({ src, width, quality }) =>
-    `https://sharpify.vercel.app/p?src=${origin}${src}&q=${
-      quality || 80
-    }&w=${width}`;
+const loader: ImageLoader = ({ src, width, quality }) =>
+  `https://sharpify.vercel.app/p?src=${
+    process.env.NEXT_PUBLIC_ORIGIN
+  }${src}&q=${quality || 80}&w=${width}`;
 
 export const Image = (props: ImageProps) => {
-  const [origin, setOrigin] = useState(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setOrigin(window.location.origin);
-    }
-  }, []);
-
   const customProps = useMemo(() => {
     return process.env.NODE_ENV === 'development'
       ? {
           unoptimized: true,
         }
       : {
-          loader: loader(origin),
+          loader,
         };
-  }, [origin]);
+  }, []);
 
   return <NextImage {...props} {...customProps} />;
 };
